28043098ec6a7fb65ffdaea043b92cf2e9a15f7c
HiteshAmbarkhane <hiteshtheone@gmail.com>
2026-04-20 16:07:32 +0530

    First Django change

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+[New with Django]
+Project = Ironcrew
+App1 = homePage
+
+
+[OLD]
 To setup dev environment -
 
 Install git, python3, virtual env
@@ -11,3 +17,5 @@ pip install -r requirements.txt
 Run this to start the website -
 
 uvicorn main:app --reload --host 0.0.0.0 --port 8000
+
+
---
+++ djangoAkola/ironcrew/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ironcrew project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ironcrew.settings')
+
+application = get_asgi_application()
