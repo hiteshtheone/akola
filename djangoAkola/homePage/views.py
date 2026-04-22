4c92b97d5c2edcb855f0d0974c89698884d72f0a
HiteshAmbarkhane <hiteshtheone@gmail.com>
2026-04-22 14:22:34 +0530

    HomePage is up

--- djangoAkola/homePage/views.py
+++ djangoAkola/homePage/views.py
@@ -1,3 +1,9 @@
+# Create your views here.
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {}
+    return render(request, "homePage/index.html", context)
+    #return HttpResponse("Hello, world. You're at the HomePage index.")
